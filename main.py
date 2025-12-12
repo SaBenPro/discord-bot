e798661ea2c507e61385012c22a50629215f8132
SaBenPro <ertanyigity@gmail.com>
2025-12-12 15:13:42 +0300

    main.py

--- main.py
+++ main.py
@@ -3,19 +3,15 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.all()
-
 bot = commands.Bot(command_prefix="!", intents=intents)
-
 
 @bot.event
 async def on_ready():
     print(f"Bot aktif: {bot.user}")
 
-
 @bot.command()
 async def merhaba(ctx):
     await ctx.send("Selam knk! Bot çalışıyor 👋")
-
 
 TOKEN = os.getenv("TOKEN")
 
